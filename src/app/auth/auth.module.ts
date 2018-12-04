09ee3893f93be7352b6a6cff6cdc4e05cee2d9e4
Ivan Liauchuk <ivan.liauchuk@gmail.com>
2018-12-03 22:52:06 -0500

    Added login and sign up template. Added shared and system state skeleton modules.

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {AuthComponent} from './auth.component';
+import {AuthRoutingModule} from './auth-routing.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+    AuthComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule {
+}
